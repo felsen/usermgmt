5a7d059e7cf4e78ba85e7f18920996f30be7bc3e
felsen <felsen88@gmail.com>
2016-04-13 21:23:14 +0000

    Added: Database Tables are created

---
+++ usermanagement/models.py
@@ -0,0 +1,100 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+
+
+class Base(models.Model):
+    """
+    Common database fields are under base tables(Inheritance).
+    """
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Menus(Base):
+    """
+    Menu and sub menu has added by the user
+    """
+    name = models.CharField("Menu Name", max_length=50, blank=True, null=True)
+    slug = models.SlugField("SEO friendly URL, use alphabets and hyphen", blank=True, null=True)
+    parent = models.ForeignKey("self", blank=True, null=True)
+    link = models.CharField("Link", max_length=20, blank=True, null=True)
+    active = models.IntegerField(default=2)
+    created_by = models.ForeignKey(User, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Menu"
+
+    def __unicode__(self):
+        return "%s" % (self.name)
+
+    def get_submenu(self):
+        return Menus.objects.filter(parent=self, active=2)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Menus, self).save(*args, **kwargs)
+
+
+class Role_Types(Base):
+    """
+    User Type has stored in this table Ex: Employee, Customer, etc,...
+    """
+    name = models.CharField(max_length=50, blank=True, null=True)
+    slug = models.SlugField("SEO friendly URL, use alphabets and hyphen", unique=True, blank=True, null=True)
+    created_by = models.ForeignKey(User, blank=True, null=True)
+    active = models.IntegerField(default=2)
+
+    class Meta:
+        verbose_name = "Role Type"
+
+    def __unicode__(self):
+        return "%s" % (self.name)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Role_Types, self).save(*args, **kwargs)
+
+
+class User_Roles(Base):
+    """
+    User and the role type has stored in this table.
+    """
+    user = models.ForeignKey(User, blank=True, null=True)
+    role_type = models.ManyToManyField(Role_Types, related_name="role_type_related_to_user_%(class)s")
+    active = models.IntegerField(default=2)
+    created_by = models.ForeignKey(User, blank=True, null=True, related_name="User_related_to_created_by_%(class)s")
+    modified_by = models.ForeignKey(User, blank=True, null=True, related_name="User_related_to_modified_by_%(class)s")
+
+    class Meta:
+        verbose_name = "User Role"
+
+    def __unicode__(self):
+        return "%s" % (self.user.name)
+
+
+class Role_Configuration(Base):
+    """
+    Assigning the menu based permission in this table.
+    """
+    role = models.ForeignKey(Role_Types)
+    menu = models.ForeignKey(Menus)
+    add = models.IntegerField(blank=True, null=True)
+    edit = models.IntegerField(blank=True, null=True)
+    view = models.IntegerField(blank=True, null=True)
+    delete = models.IntegerField(blank=True, null=True)
+    search = models.IntegerField(blank=True, null=True)
+    generate_report = models.IntegerField(blank=True, null=True)
+    active = models.IntegerField(default=2)
+    created_by = models.ForeignKey(User, blank=True, null=True, related_name="User_related_to_created_by_%(class)s")
+    modified_by = models.ForeignKey(User, blank=True, null=True, related_name="User_related_to_modified_by_%(class)s")
+
+    class Meta:
+        verbose_name = "Role Configuration"
+
+    def __unicode__(self):
+        return "%s - %s" % (self.role.name, self.menu.name)
+
