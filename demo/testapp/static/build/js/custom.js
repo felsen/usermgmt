b53edb2b362cbab4f971075957eca93749c36392
FeLiX StEpHeN <felsen88@gmail.com>
2016-10-20 15:23:49 +0530

    Added: customer javascript for slideup/slidedown the submenus

--- demo/testapp/static/build/js/custom.js
+++ demo/testapp/static/build/js/custom.js
@@ -47,7 +47,8 @@ var CURRENT_URL = window.location.href.split('?')[0],
     $LEFT_COL = $('.left_col'),
     $RIGHT_COL = $('.right_col'),
     $NAV_MENU = $('.nav_menu'),
-    $FOOTER = $('footer');
+    $FOOTER = $('footer'),
+    $SIDEDOWN_CARRET = $('.fa-chevron-down');
 
 // Sidebar
 $(document).ready(function() {
@@ -70,6 +71,28 @@ $(document).ready(function() {
     $SIDEBAR_MENU.find('a').on('click', function(ev) {
         var $li = $(this).parent();
 
+        if ($li.is('.active')) {
+            $li.removeClass('active active-sm');
+            // $('ul:first', $li).slideUp(function() {
+            //     setContentHeight();
+            // });
+        } else {
+            // prevent closing menu if we are on child menu
+            if (!$li.parent().is('.child_menu')) {
+                $SIDEBAR_MENU.find('li').removeClass('active active-sm');
+                $SIDEBAR_MENU.find('li ul').slideUp();
+            }
+            
+            $li.addClass('active');
+
+            // $('ul:first', $li).slideDown(function() {
+            //     setContentHeight();
+            // });
+        }
+    });
+
+    $SIDEDOWN_CARRET.on('click', function(ev) {
+        var $li = $(this).parent().parent();
         if ($li.is('.active')) {
             $li.removeClass('active active-sm');
             $('ul:first', $li).slideUp(function() {
@@ -106,13 +129,13 @@ $(document).ready(function() {
     });
 
     // check active menu
-    $SIDEBAR_MENU.find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
+    // $SIDEBAR_MENU.find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
 
-    $SIDEBAR_MENU.find('a').filter(function () {
-        return this.href == CURRENT_URL;
-    }).parent('li').addClass('current-page').parents('ul').slideDown(function() {
-        setContentHeight();
-    }).parent().addClass('active');
+    // $SIDEBAR_MENU.find('a').filter(function () {
+    //     return this.href == CURRENT_URL;
+    // }).parent('li').addClass('current-page').parents('ul').slideDown(function() {
+    //     setContentHeight();
+    // }).parent().addClass('active');
 
     // recompute content when resizing
     $(window).smartresize(function(){  
